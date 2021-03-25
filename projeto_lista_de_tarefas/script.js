261a0c7623dc12eecdc950619e694683d6c7f0cb
LeonarDev <leonardo.majevski@gmail.com>
2021-03-25 16:37:48 -0300

    Feature: Adds a button to complete a task

--- projeto_lista_de_tarefas/script.js
+++ projeto_lista_de_tarefas/script.js
@@ -9,11 +9,31 @@ const criarTarefa = (evento) => {
   
   tarefa.innerHTML = conteudo;
   tarefa.classList.add('task');
+  tarefa.appendChild(BotaoConclui());
   lista.appendChild(tarefa);
 
   input.value = '';
 }
 
 const novaTarefa = document.querySelector('[data-form-button]');
-
 novaTarefa.addEventListener('click', criarTarefa);
+
+const BotaoConclui = () => {
+  const botaoConclui = document.createElement('button');
+
+  botaoConclui.classList.add('check-button');
+  botaoConclui.innerHTML = 'concluir'
+  botaoConclui.addEventListener('click', () => {
+
+  });
+
+  return botaoConclui;
+}
+
+const concluirTarefa = (evento) => {
+  const botaoConclui = evento.target
+
+  const tarefaCompleta = botaoConclui.parentElement
+
+  tarefaCompleta.classList.toggle('done');
+}

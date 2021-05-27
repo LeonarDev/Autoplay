b1235259a31e2a8fe632080d9ca531a9103112dd
LeonarDev <leonardo.majevski@gmail.com>
2021-05-27 08:53:52 -0300

    Cria sistema de autenticação

---
+++ 2_back-end/bytebank/Autenticacao.js
@@ -0,0 +1,13 @@
+export default class Autenticacao {
+  static login(autenticavel, senha) {
+    if(Autenticacao.ehAutenticavel(autenticavel)) {
+      return autenticavel.autenticar(senha);
+    }
+    return false;
+  }
+
+  static ehAutenticavel(autenticavel) {
+    return "autenticar" in autenticavel && 
+    autenticavel.autenticar instanceof Function
+  }
+}
--- 2_back-end/bytebank/index.js
+++ 2_back-end/bytebank/index.js
@@ -1,38 +1,7 @@
-import Cliente from './Cliente.js';
-import ContaCorrente from './contas/ContaCorrente.js';
-import ContaPoupanca from './contas/ContaPoupanca.js';
-
-const CLIENTE_1 = new Cliente("Ricardo", 11122233309);
-const CLIENTE_2 = new Cliente("Alice", 88822233309);
-
-const CONTA_1 = new ContaCorrente(1001, CLIENTE_1);
-const CONTA_2 = new ContaPoupanca(0, 1001, CLIENTE_2);
-
-console.log('---------------------------------------------------------------------');
-console.log('');
-console.log('CONTA INICIAL:');
-console.log('');
-console.log(CONTA_1);
-
-console.log('');
-console.log('---------------------------------------------------------------------');
-console.log('');
-
-
-console.log('DEPÓSITO DE VALOR NEGATIVO:');
-console.log('');
-CONTA_1.depositar(-500);
-
-console.log('');
-console.log(CONTA_1);
-
-console.log('');
-console.log('---------------------------------------------------------------------');
-console.log('');
-
-console.log('DEPÓSITO DE VALOR POSITIVO:');
-console.log('');
-CONTA_1.depositar(200);
-
-console.log('');
-console.log(CONTA_1);
+// import Cliente from './Cliente.js';
+// import ContaCorrente from './contas/ContaCorrente.js';
+// import ContaPoupanca from './contas/ContaPoupanca.js';
+// import ContaSalario from './contas/ContaSalario.js';
+// import Diretor from './funcionarios/Diretor.js';
+// import Gerente from './funcionarios/Gerente.js';
+// import Autenticacao from './Autenticacao.js';

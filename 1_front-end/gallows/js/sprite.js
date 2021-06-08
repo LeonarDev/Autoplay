b18eeb9fa0854263003a218c1447225d41767abc
LeonarDev <leonardo.majevski@gmail.com>
2021-06-08 10:19:37 -0300

    Requisito 2: Implementa botão de reset e variável isFinished

--- 1_front-end/gallows/js/sprite.js
+++ 1_front-end/gallows/js/sprite.js
@@ -1,7 +1,18 @@
+// var sprite = createSprite('.sprite');
+
 let divSprite = document.querySelector('.sprite');
 let frameAtual = 1;
 let frameFinal = 9;
+let isFinished = false;
 
 setInterval(() => {
-  if (frameAtual <= frameFinal) divSprite.classList.add(`frame${frameAtual += 1}`)
+  let nextFrame = `frame${frameAtual += 1}`
+  if (frameAtual <= frameFinal) divSprite.classList.add(nextFrame);
+  if (frameAtual == frameFinal) isFinished = true;
 }, 500);
+
+
+let resetButton = document.querySelector('#reset-game');
+resetButton.addEventListener('click', () => {
+  window.location.reload()
+})

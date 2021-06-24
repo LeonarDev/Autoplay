2066bf845fe430a9ebcef70743891e2c1c3c096b
LeonarDev <leonardo.majevski@gmail.com>
2021-06-24 10:51:26 -0300

    Exercicios C#

--- 2_back-end/cSharp/ByteBank/01-ByteBank/GerenciadorBonificacao.cs
+++ 2_back-end/cSharp/ByteBank/01-ByteBank/GerenciadorBonificacao.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ByteBank.Funcionarios;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,15 @@ using System.Threading.Tasks;
 
 namespace ByteBank
 {
-    class GerenciadorBonificacao
+    public class GerenciadorBonificacao
     {
+        private double _totalBonificacao;
+        public void Registrar(Funcionario funcionario) {
+            _totalBonificacao += funcionario.GetBonificacao();
+        }
+
+        public double GetTotalBonificacao() {
+            return _totalBonificacao;
+        }
     }
 }
--- 2_back-end/cSharp/ByteBank/01-ByteBank/Program.cs
+++ 2_back-end/cSharp/ByteBank/01-ByteBank/Program.cs
@@ -1,21 +1,80 @@
-﻿using System;
+﻿using ByteBank.Funcionarios;
+using ByteBank.Sistemas;
+using System;
 
 namespace ByteBank
 {
     class Program
     {
-       static void Main(string[] args)
+       static void Main(string[] args) {
+            Console.WriteLine("Total de contas: " + ContaCorrente.totalDeContas);
+
+            ContaCorrente conta1 = new ContaCorrente(867, 86712540);
+            Console.WriteLine("Total de contas: " + ContaCorrente.totalDeContas);
+
+            Console.WriteLine("GetAgencia da conta1: " + conta1.Agencia); //getter
+            Console.WriteLine("GetNumero da conta1: " + conta1.Numero); //getter
+
+            ContaCorrente conta2 = new ContaCorrente(867, 86745820);
+            Console.WriteLine("Total de contas: " + ContaCorrente.totalDeContas);
+
+            Console.WriteLine("----------------");
+
+            CalcularBonificacao();
+
+            Console.WriteLine("----------------");
+
+            UsarSistema();
+        }
+
+        public static void UsarSistema()
         {
-            Console.WriteLine(ContaCorrente.totalDeContas);
+            SistemaInterno sistemaInterno = new SistemaInterno();
 
-            ContaCorrente conta = new ContaCorrente(867, 86712540);
-            Console.WriteLine(ContaCorrente.totalDeContas);
+            Diretor roberta = new Diretor("159.753.398-04");
+            roberta.Nome = "Roberta";
+            roberta.Senha = "123";
 
-            Console.WriteLine(conta.Agencia);
-            Console.WriteLine(conta.Numero);
+            GerenteDeConta camila = new GerenteDeConta("326.985.628-89");
+            camila.Nome = "Camila";
+            camila.Senha = "abc";
 
-            ContaCorrente outraConta = new ContaCorrente(867, 86745820);
-            Console.WriteLine(ContaCorrente.totalDeContas);
+            ParceiroComercial parceiro = new ParceiroComercial();
+            parceiro.Senha = "123456";
+
+            sistemaInterno.Logar(parceiro, "123456");
+
+            sistemaInterno.Logar(roberta, "123");
+            sistemaInterno.Logar(camila, "abc");
+        }
+
+        public static void CalcularBonificacao()
+        {
+            GerenciadorBonificacao gerenciadorBonificacao = new GerenciadorBonificacao();
+
+            Funcionario pedro = new Designer("833.222.048-39");
+            pedro.Nome = "Pedro";
+
+            Funcionario roberta = new Diretor("159.753.398-04");
+            roberta.Nome = "Roberta";
+
+            Funcionario igor = new Auxiliar("981.198.778-53");
+            igor.Nome = "Igor";
+
+            Funcionario camila = new GerenteDeConta("326.985.628-89");
+            camila.Nome = "Camila";
+
+            Desenvolvedor guilherme = new Desenvolvedor("456.175.468-20");
+            guilherme.Nome = "Guilherme";
+
+            gerenciadorBonificacao.Registrar(guilherme);
+            gerenciadorBonificacao.Registrar(pedro);
+            gerenciadorBonificacao.Registrar(roberta);
+            gerenciadorBonificacao.Registrar(igor);
+            gerenciadorBonificacao.Registrar(camila);
+
+            Console.WriteLine("Total de bonificações do mês " +
+            gerenciadorBonificacao.GetTotalBonificacao());
         }
     }
 }

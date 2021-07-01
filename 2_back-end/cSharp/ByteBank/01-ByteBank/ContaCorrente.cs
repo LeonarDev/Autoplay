325c3ca1d6290256481fd03e8687fc1afd9eaa03
LeonarDev <leonardo.majevski@gmail.com>
2021-07-01 15:59:35 -0300

    Adc exceções

--- 2_back-end/cSharp/ByteBank/01-ByteBank/ContaCorrente.cs
+++ 2_back-end/cSharp/ByteBank/01-ByteBank/ContaCorrente.cs
@@ -1,43 +1,86 @@
 ﻿// using ByteBank; linha necessária para identificar o namespace caso não contenha no arquivo
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace ByteBank
 {
     public class ContaCorrente
     {
-        public ContaCorrente(int agencia, int numero) {
+        public Cliente Titular { get; set; }
+        public static int totalDeContas { get; private set; }
+        private static int TaxaOperacao;
+        public int ContadorSaquesInvalidos { get; private set; }
+        public int ContadorTransferenciasInvalidas { get; private set; }
+        public int Agencia { get; }
+        public int Numero { get; }
+        private double _saldo;
+        public double Saldo
+        {
+            get
+            {
+            return this._saldo;
+            }
+            set
+            {
+                if (value < 0)
+                {
+                    return;
+                }
+
+                _saldo = value;
+            }
+        }
+
+        public ContaCorrente(int agencia, int numero)
+        {
+            if (agencia <= 0) throw new ArgumentException("O argumento 'Agencia' deve ser maior que ZERO.", nameof(agencia));
+            if (numero <= 0) throw new ArgumentException("O argumento 'Numero' deve ser maior que ZERO.", nameof(numero));
+
             Agencia = agencia;
             Numero = numero;
             totalDeContas++;
+            TaxaOperacao = 30 / totalDeContas;
         }
-
-        public static int totalDeContas { get; private set; }
-
-        public Cliente Titular { get; set; }
-        public int Agencia { get; set; }
-        public int Numero { get; set; }
-        private double _saldo;
-
-        public double getSaldo() { return this._saldo; }
 
         public void Depositar(double valor) {
             if (valor > 0) this._saldo += valor;
         }
 
-        public bool Sacar(double valor) {
-            if (this._saldo < valor) {
-                return false;
-            } 
+        public void Sacar(double valor)
+        {
+            if (valor < 0) throw new ArgumentException("Valor inválido para o saque.", nameof(valor));
+
+            if (this._saldo < valor)
+            {
+                ContadorSaquesInvalidos++;
+                throw new SaldoInsuficienteException(Saldo, valor);
+            }
+
             this._saldo -= valor;
-            return true;
         }
 
-        public bool Transferir(double valor, ContaCorrente contaDestino) {
-            if (this._saldo < valor) {
-                return false;
-            } 
-            this._saldo -= valor;
+        public void Transferir(double valor, ContaCorrente contaDestino)
+        {
+            if (valor < 0)
+            {
+                ContadorTransferenciasInvalidas++;
+                throw new ArgumentException("Valor inválido para a transferência.", nameof(valor));
+            }
+
+            try
+            {
+                Sacar(valor);
+            }
+            catch(SaldoInsuficienteException ex)
+            {
+                ContadorTransferenciasInvalidas++;
+                throw new OperacaoFinanceiraException("Operação não realizada.", ex);
+            }
+
             contaDestino.Depositar(valor);
-            return true;
         }
     }
 }

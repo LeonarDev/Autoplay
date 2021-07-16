86309ce2b153f65e01c75bce5546cebfeddfa0e3
LeonarDev <leonardo.majevski@gmail.com>
2021-07-15 23:45:44 -0300

    Lidando com SortedDictionary

---
+++ 2_back-end/cSharp/Collections/partTwo/SortedDictionary/Aluno.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortedDictionary
+{
+    class Aluno
+    {
+        private string nome;
+        public string Nome
+        {
+            get { return nome; }
+        }
+
+        private int numeroMatricula;
+        public int NumeroMatricula
+        {
+            get { return numeroMatricula; }
+        }
+
+        public Aluno(String nome, int numeroMatricula)
+        {
+            this.nome = nome;
+            this.numeroMatricula = numeroMatricula;
+        }
+
+        public override string ToString()
+        {
+            return "[Aluno: " + this.nome + ", matricula: " + this.numeroMatricula + "]";
+        }
+
+        public override bool Equals(object obj)
+        {
+            Aluno outro = obj as Aluno;
+
+            if (outro == null)
+            {
+                return false;
+            }
+            return this.nome.Equals(outro.nome);
+        }
+
+        //importante: rapidez da busca depende do CÓDIGO DE DISPERSÃO!
+        public override int GetHashCode()
+        {
+            return this.nome.GetHashCode();
+        }
+    }
+}
---
+++ 2_back-end/cSharp/Collections/partTwo/SortedDictionary/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SortedDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IDictionary<string, Aluno> sorted = new SortedDictionary<string, Aluno>();
+
+            sorted.Add("VT", new Aluno("Vanessa", 34672));
+            sorted.Add("AL", new Aluno("Ana", 5617));
+            sorted.Add("RN", new Aluno("Rafael", 17645));
+            sorted.Add("WM", new Aluno("Wanderson", 11287));
+
+            foreach (var item in sorted)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}

e02aa6699ed14bffb4da3817f3764ac7ad9c0cb7
LeonarDev <leonardo.majevski@gmail.com>
2021-07-17 11:28:29 -0300

    Lidando com JaggedArrays (Arrays denteados - arrays de arrays)

---
+++ 2_back-end/cSharp/Collections/partTwo/JaggedArrays/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace JaggedArrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[][] familias = new string[3][];
+
+            familias[0] = new string[] { "Fred", "Wilma", "Pedrita" };
+            familias[1] = new string[] { "Homer", "Marge", "Lisa", "Bart", "Maggie" };
+            familias[2] = new string[] { "Florinda", "Kiko" };
+
+            int contador = 1;
+
+            foreach(var familia in familias)
+            {
+                Console.WriteLine($"Família {contador} : { string.Join(", ", familia)}");
+                contador++;
+            }
+        }
+    }
+}

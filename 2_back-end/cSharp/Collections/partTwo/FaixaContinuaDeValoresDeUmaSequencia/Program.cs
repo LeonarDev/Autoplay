ca3f4732ac9b64208073cf2a21576efc1f8d3ad0
LeonarDev <leonardo.majevski@gmail.com>
2021-07-18 12:48:04 -0300

    Format

--- 2_back-end/cSharp/Collections/partTwo/FaixaContinuaDeValoresDeUmaSequencia/Program.cs
+++ 2_back-end/cSharp/Collections/partTwo/FaixaContinuaDeValoresDeUmaSequencia/Program.cs
@@ -10,13 +10,14 @@ namespace FaixaContinuaDeValoresDeUmaSequencia
             // Uma loja de fast-food tem um faturamento que varia dia a dia.
             // Na última semana, essa loja registrou os seguintes faturamentos:
             var dias = new[] {
-            new { nome = "segunda", faturamento = 1000 },
-            new { nome = "terça", faturamento = 2000 },
-            new { nome = "quarta", faturamento = 12500 },
-            new { nome = "quinta", faturamento = 11000 },
-            new { nome = "sexta", faturamento = 22000 },
-            new { nome = "sábado", faturamento = 9000 },
-            new { nome = "domingo", faturamento = 18000 }};
+                new { nome = "segunda", faturamento = 1000 },
+                new { nome = "terça", faturamento = 2000 },
+                new { nome = "quarta", faturamento = 12500 },
+                new { nome = "quinta", faturamento = 11000 },
+                new { nome = "sexta", faturamento = 22000 },
+                new { nome = "sábado", faturamento = 9000 },
+                new { nome = "domingo", faturamento = 18000 }
+            };
 
             // Essa loja quer saber:
             // quais dias consecutivos tiveram faturamento igual ou superior a 10 mil reais?

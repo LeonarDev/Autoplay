4d5bf55ac10e47abccd7be466265e70ab5e72570
LeonarDev <leonardo.majevski@gmail.com>
2021-07-19 12:24:29 -0300

    Retornando endereço com webservice de consultar CEP

---
+++ 2_back-end/cSharp/CEP/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Net.Http;
+
+namespace CEP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // webservice para consultar CEP: http://viacep.com.br/
+            // exemplo: http://viacep.com.br/ws/01001000/json/
+
+            string cep = "01001000";
+            string url = $"http://viacep.com.br/ws/{cep}/json/";
+
+            string result = new HttpClient().GetStringAsync(url).Result;
+
+            Console.WriteLine($"Resultado da url {url} :");
+            Console.WriteLine();
+            Console.WriteLine(result);
+        }
+    }
+}

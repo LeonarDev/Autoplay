86cf6ebb216ba19f496d4a6c6ef1828fb3407d16
LeonarDev <leonardo.majevski@gmail.com>
2021-07-19 09:08:46 -0300

    Validando documentos com lib externa (via nuGet)

--- 2_back-end/cSharp/ValidadorDeDocumentos/Program.cs
+++ 2_back-end/cSharp/ValidadorDeDocumentos/Program.cs
@@ -9,18 +9,64 @@ namespace ValidadorDeDocumentos
         static void Main(string[] args)
         {
             string cpf1 = "86288366757";
-            //string cpf2 = "98745366797";
-            //string cpf3 = "22222222222";
+            string cpf2 = "98745366797";
 
+            string cnpj1 = "17175450133770";
+            string cnpj2 = "45700718735752";
 
+            string tituloEleitor1 = "31550558143";
+            string tituloEleitor2 = "777541881422";
+
+            ValidarCPF(cpf1);
+            ValidarCPF(cpf2);
+
+            ValidarCNPJ(cnpj1);
+            ValidarCNPJ(cnpj2);
+
+            ValidarTituloEleitoral(tituloEleitor1);
+            ValidarTituloEleitoral(tituloEleitor2);
+        }
+
+        private static void ValidarCPF(string cpf)
+        {
             try
             {
-                new CPFValidator().AssertValid(cpf1);
-                Debug.WriteLine($"CPF válido: {cpf1}");
+                new CPFValidator().AssertValid(cpf);
+                Console.WriteLine($"CPF válido: {cpf}");
+                Console.WriteLine();
             }
             catch (Exception exc)
             {
-                Debug.WriteLine($"CPF inválido: {cpf1} : {exc}");
+                Console.WriteLine($"CPF inválido: {cpf} : {exc.ToString()}");
+                Console.WriteLine();
+            }
+        }
+
+        private static void ValidarCNPJ(string cnpj)
+        {
+            if (new CNPJValidator().IsValid(cnpj))
+            {
+                Console.WriteLine($"CNPJ válido: {cnpj}");
+                Console.WriteLine();
+            }
+            else
+            {
+                Console.WriteLine($"CNPJ inválido: {cnpj}");
+                Console.WriteLine();
+            }
+        }
+
+        private static void ValidarTituloEleitoral(string tituloEleitor)
+        {
+            if (new TituloEleitoralValidator().IsValid(tituloEleitor))
+            {
+                Console.WriteLine($"Título Eleitoral válido: {tituloEleitor}");
+                Console.WriteLine();
+            }
+            else
+            {
+                Console.WriteLine($"Título Eleitoral inválido: {tituloEleitor}");
+                Console.WriteLine();
             }
         }
     }

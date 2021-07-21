048c2cf2fbc98f87c7d9817073b41b6a0d507efe
LeonarDev <leonardo.majevski@gmail.com>
2021-07-20 21:57:04 -0300

    Lê arquivo de texto e lida com encoding

---
+++ 2_back-end/cSharp/in-out/ByteBankImportacaoExportacao/Program.cs
@@ -0,0 +1,45 @@
+﻿using ByteBankImportacaoExportacao.Modelos;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBankImportacaoExportacao 
+{ 
+    class Program 
+    { 
+        static void Main(string[] args) 
+        {
+            var enderecoDoArquivo = "contas.txt";
+
+            var fluxoDoArquivo = new FileStream(enderecoDoArquivo, FileMode.Open);
+
+            var buffer = new byte[1024];
+            var numeroDeBytesLidos = -1;
+
+            while (numeroDeBytesLidos != 0)
+            {
+                numeroDeBytesLidos = fluxoDoArquivo.Read(buffer, 0, 1024);
+                EscreverBuffer(buffer);
+            }
+
+            Console.ReadLine();
+        }
+
+        private static void EscreverBuffer(byte[] buffer)
+        {
+            var utf8 = Encoding.UTF8;
+            var texto = utf8.GetString(buffer);
+            Console.Write(texto);
+
+            //foreach (var byteLido in buffer)
+            //{
+            //    Console.Write(byteLido);
+            //    Console.Write(" ");
+            //}
+        }
+    }
+} 
+ 

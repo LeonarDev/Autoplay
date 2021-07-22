be6cbae8c7095f77a19b80e9ccf26a81d33232cb
LeonarDev <leonardo.majevski@gmail.com>
2021-07-21 21:27:46 -0300

    Converte tipo string para tipo ContaCorrente

--- 2_back-end/cSharp/in-out/ByteBankImportacaoExportacao/Program.cs
+++ 2_back-end/cSharp/in-out/ByteBankImportacaoExportacao/Program.cs
@@ -24,11 +24,37 @@ namespace ByteBankImportacaoExportacao
                 while (!leitor.EndOfStream)
                 {
                     var linha = leitor.ReadLine();
-                    Console.WriteLine(linha);
+                    var contaCorrente = ConverteStringParaContaCorrente(linha);
+
+                    Console.WriteLine($"Conta Corrente => Número: {contaCorrente.Numero} | Agência: {contaCorrente.Agencia} | Saldo: {contaCorrente.Saldo} | Titular: {contaCorrente.Titular.Nome}");
+                    //Console.WriteLine(linha);
                 }
             }
 
             Console.ReadLine();
+        }
+
+        static ContaCorrente ConverteStringParaContaCorrente(string linha)
+        {
+            string[] camposDocumento = linha.Split(' ');
+
+            var agenciaString = camposDocumento[0];
+            var numeroString = camposDocumento[1];
+            var saldoString = camposDocumento[2].Replace('.', ',');
+            var nomeTitular = camposDocumento[3];
+
+            var agenciaInt = int.Parse(agenciaString);
+            var numeroInt = int.Parse(numeroString);
+            var saldoDouble = double.Parse(saldoString);
+
+            var titular = new Cliente();
+            titular.Nome = nomeTitular;
+
+            var resultado = new ContaCorrente(agenciaInt, numeroInt);
+            resultado.Depositar(saldoDouble);
+            resultado.Titular = titular;
+
+            return resultado;
         }
     }
 } 

2b06fbe1be2f0f724d0a53b0f347523d15ad2cdd
LeonarDev <leonardo.majevski@gmail.com>
2021-07-26 19:17:17 -0300

    Cria métodos de teste do xUnit através da notação [Fact]

---
+++ 2_back-end/cSharp/Alura.LeilaoOnline/Alura.LeilaoOnline.ConsoleApp/Program.cs
@@ -0,0 +1,14 @@
+﻿using Alura.LeilaoOnline.Core;
+using System;
+
+namespace Alura.LeilaoOnline.ConsoleApp
+{
+    class Program
+    {
+        static void Main()
+        {
+            var leilao = new Leilao("Van Gogh");
+            var fulano = new Interessada("Fulano", leilao);
+        }
+    }
+}

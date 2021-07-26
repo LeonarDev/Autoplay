affa0cec1184ca9df3f5bad014ddac18fa7e1c90
LeonarDev <leonardo.majevski@gmail.com>
2021-07-26 17:10:56 -0300

    Utiliza stream de entrada do Console e auxiliares na classe 'File'

---
+++ 2_back-end/cSharp/in-out/ByteBankImportacaoExportacao/2_UsandoStreamReader.cs
@@ -0,0 +1,55 @@
+﻿using ByteBankImportacaoExportacao.Modelos;
+using System;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBankImportacaoExportacao
+{
+    partial class Program
+    {
+        static void UserStreamReader()
+        {
+            var enderecoDoArquivo = "contas.txt";
+
+            using (var fluxoDoArquivo = new FileStream(enderecoDoArquivo, FileMode.Open))
+            using (var leitor = new StreamReader(fluxoDoArquivo))
+            {
+                //var linha = leitor.ReadToEnd();  -> Carrega todo o conteúdo do arquivo de uma só vez (pode travar a aplicação) É recomendável não usar
+
+                while (!leitor.EndOfStream)
+                {
+                    var linha = leitor.ReadLine();
+                    var contaCorrente = ConverteStringParaContaCorrente(linha);
+
+                    Console.WriteLine($"Conta Corrente => Número: {contaCorrente.Numero} | Agência: {contaCorrente.Agencia} | Saldo: {contaCorrente.Saldo} | Titular: {contaCorrente.Titular.Nome}");
+                    //Console.WriteLine(linha);
+                }
+            }
+        }
+
+        static ContaCorrente ConverteStringParaContaCorrente(string linha)
+        {
+            string[] camposDocumento = linha.Split(',');
+
+            var agenciaString = camposDocumento[0];
+            var numeroString = camposDocumento[1];
+            var saldoString = camposDocumento[2].Replace('.', ',');
+            var nomeTitular = camposDocumento[3];
+
+            var agenciaInt = int.Parse(agenciaString);
+            var numeroInt = int.Parse(numeroString);
+            var saldoDouble = double.Parse(saldoString);
+
+            var titular = new Cliente();
+            titular.Nome = nomeTitular;
+
+            var resultado = new ContaCorrente(agenciaInt, numeroInt);
+            resultado.Depositar(saldoDouble);
+            resultado.Titular = titular;
+
+            return resultado;
+        }
+    }
+}
--- 2_back-end/cSharp/in-out/ByteBankImportacaoExportacao/Program.cs
+++ 2_back-end/cSharp/in-out/ByteBankImportacaoExportacao/Program.cs
@@ -9,52 +9,42 @@ using System.Threading.Tasks;
 namespace ByteBankImportacaoExportacao 
 { 
     partial class Program 
-    { 
+    {
         static void Main(string[] args)
         {
-            //LidandoComFileStream();
+            File.WriteAllText("escrevendoComAClasseFile.txt", "Testando File.WriteAllText");
+            Console.WriteLine("Arquivo escrevendoComAClasseFile.txt criado!");
 
-            var enderecoDoArquivo = "contas.txt";
+            var bytesAquivo = File.ReadAllBytes("contas.txt");
+            Console.WriteLine($"O arquivo contas.txt possui {bytesAquivo.Length} bytes");
 
-            using (var fluxoDoArquivo = new FileStream(enderecoDoArquivo, FileMode.Open))
-            using (var leitor = new StreamReader(fluxoDoArquivo))
+            var linhas = File.ReadAllLines("contas.txt");
+            Console.WriteLine(linhas.Length);
+
+            foreach (var linha in linhas)
             {
-                //var linha = leitor.ReadToEnd();  -> Carrega todo o conteúdo do arquivo de uma só vez (pode travar a aplicação) É recomendável não usar
-
-                while (!leitor.EndOfStream)
-                {
-                    var linha = leitor.ReadLine();
-                    var contaCorrente = ConverteStringParaContaCorrente(linha);
-
-                    Console.WriteLine($"Conta Corrente => Número: {contaCorrente.Numero} | Agência: {contaCorrente.Agencia} | Saldo: {contaCorrente.Saldo} | Titular: {contaCorrente.Titular.Nome}");
-                    //Console.WriteLine(linha);
-                }
+                Console.WriteLine(linha);
             }
 
             Console.ReadLine();
-        }
 
-        static ContaCorrente ConverteStringParaContaCorrente(string linha)
-        {
-            string[] camposDocumento = linha.Split(' ');
+            Console.WriteLine("Digite o seu nome:");
+            var nome = Console.ReadLine();
 
-            var agenciaString = camposDocumento[0];
-            var numeroString = camposDocumento[1];
-            var saldoString = camposDocumento[2].Replace('.', ',');
-            var nomeTitular = camposDocumento[3];
+            Console.WriteLine(nome);
 
-            var agenciaInt = int.Parse(agenciaString);
-            var numeroInt = int.Parse(numeroString);
-            var saldoDouble = double.Parse(saldoString);
 
-            var titular = new Cliente();
-            titular.Nome = nomeTitular;
+            UsarStreamDeEntrada();
 
-            var resultado = new ContaCorrente(agenciaInt, numeroInt);
-            resultado.Depositar(saldoDouble);
-            resultado.Titular = titular;
+            Console.WriteLine("Aplicação finalizada. . .");
 
-            return resultado;
+            Console.ReadLine();
+
+            //EscritaBinaria();
+            //LeituraBinaria();
+            //Console.WriteLine("Aplicação finalizada.");
+
+            //Console.ReadLine();
         }
     }
 } 

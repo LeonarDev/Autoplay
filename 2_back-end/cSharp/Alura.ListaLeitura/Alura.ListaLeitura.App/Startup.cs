9bbf392d29e615b9107dbba9e55c4b3df0b151a1
LeonarDev <leonardo.majevski@gmail.com>
2021-07-27 12:36:29 -0300

    Inicia servidor e rotas

---
+++ 2_back-end/cSharp/Alura.ListaLeitura/Alura.ListaLeitura.App/Startup.cs
@@ -0,0 +1,40 @@
+﻿using Alura.ListaLeitura.App.Repositorio;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Alura.ListaLeitura.App
+{
+    public class Startup
+    {
+        public void Configure(IApplicationBuilder app)
+        {
+            app.Run(Roteamento);
+        }
+
+        public Task Roteamento(HttpContext context)
+        {
+            var _repo = new LivroRepositorioCSV();
+            var rotas = new Dictionary<string, string>
+            {
+                { "/Livros/ParaLer", _repo.ParaLer.ToString() },
+                { "/Livros/Lendo", _repo.Lendo.ToString() },
+                { "/Livros/Lidos", _repo.Lidos.ToString() }
+            };
+
+            if (rotas.ContainsKey(context.Request.Path))
+            {
+                return context.Response.WriteAsync(rotas[context.Request.Path]);
+            }
+
+            return context.Response.WriteAsync("Erro 404: Pagina nao encontrada!");
+        }
+
+        public Task LivrosParaLer(HttpContext context)
+        {
+            var _repo = new LivroRepositorioCSV();
+            return context.Response.WriteAsync(_repo.ParaLer.ToString());
+        }
+    }
+}
